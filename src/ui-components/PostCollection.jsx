e872d64f0664c3899be1dc767bf5183adaac59e4
J Chris Kaltwasser <chris@chrisk.com>
2023-01-13 22:02:19 -0500

    init commit

---
+++ src/ui-components/PostCollection.jsx
@@ -0,0 +1,46 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { Post } from "../models";
+import { SortDirection } from "@aws-amplify/datastore";
+import {
+  getOverrideProps,
+  useDataStoreBinding,
+} from "@aws-amplify/ui-react/internal";
+import SocialPost from "./SocialPost";
+import { Collection } from "@aws-amplify/ui-react";
+export default function PostCollection(props) {
+  const { items: itemsProp, overrideItems, overrides, ...rest } = props;
+  const itemsPagination = {
+    sort: (s) => s.createdAt(SortDirection.DESCENDING),
+  };
+  const itemsDataStore = useDataStoreBinding({
+    type: "collection",
+    model: Post,
+    pagination: itemsPagination,
+  }).items;
+  const items = itemsProp !== undefined ? itemsProp : itemsDataStore;
+  return (
+    <Collection
+      type="list"
+      direction="column"
+      justifyContent="left"
+      items={items || []}
+      {...rest}
+      {...getOverrideProps(overrides, "PostCollection")}
+    >
+      {(item, index) => (
+        <SocialPost
+          post={item}
+          key={item.id}
+          {...(overrideItems && overrideItems({ item, index }))}
+        ></SocialPost>
+      )}
+    </Collection>
+  );
+}
